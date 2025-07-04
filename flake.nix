66719c1b54b3e1c670bbac8863c74550b0fa07ac
Ludwig Connell <TorresCervantesLE@gmail.com>
2025-07-03 18:24:38 -0600

    Update flake.nix (rust build)

--- flake.nix
+++ flake.nix
@@ -7,7 +7,8 @@
 
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default =
-      nixpkgs.legacyPackages.x86_64-linux.buildRustPackage {
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.rustPlatform.buildRustPackage {
         pname = "easygit";
         version = "0.1.0";
         src = ./.;
